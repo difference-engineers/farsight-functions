2678469f803c1b3cabc7d697d040afc845f8c436
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-05-27 21:10:14 -0700

    Try telling us the logger

--- _base/configuration.rb
+++ _base/configuration.rb
@@ -2,10 +2,13 @@ NAME = ENV.fetch("NAME") {ENV.fetch("K_SERVICE")}
 VERSION = ENV.fetch("VERSION") {ENV.fetch("K_SERVICE")}
 PRODUCTION = ENV.fetch("DEPLOY_ENV") == "production"
 
+IO = if defined?(logger) then logger else $stdout end
 SemanticLogger.default_level = :trace
 SemanticLogger.add_signal_handler
-SemanticLogger.add_appender(io: if defined?(logger) then logger else $stdout end, formatter: :one_line)
+SemanticLogger.add_appender(io: IO, formatter: :one_line)
+SemanticLogger.add_appender(io: logger, formatter: :one_line)
 LOGGER = SemanticLogger[NAME]
+LOGGER.info("Using #{if defined?(logger) then "logger" else "stdout" end}")
 
 if PRODUCTION
   Google::Cloud::Debugger.configure do |config|
