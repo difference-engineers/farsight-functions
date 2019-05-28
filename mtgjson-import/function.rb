10526ff9858fdf45a4fb567889418967911c9566
Sarah Nyberg <srhbutts@gmail.com>
2019-05-28 00:21:15 +0000

    wip

--- mtgjson-import/function.rb
+++ mtgjson-import/function.rb
@@ -1,4 +1,12 @@
 def function(response:, database:)
+  uri = URI('https://mtgjson.com/json/AllCards.json')
+  response = Net::HTTP.get(uri)
+  json = JSON.parse(response)
+
+  mtgjson_imports = database.relations.fetch("mtgjson_imports")
+  changeset = mtgjson_imports.changeset(:create, {"imported_at" => Time.now, 'raw' => json })
+  changeset.commit
+
   cards = database.relations.fetch(:cards)
   response.write(cards.count.to_s)
   response.status = :ok
