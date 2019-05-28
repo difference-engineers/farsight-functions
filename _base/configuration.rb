69069044fda2e575089f833357ccfce05edd8190
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-05-27 23:48:36 -0700

    Minor touches

--- _base/configuration.rb
+++ _base/configuration.rb
@@ -1,12 +1,11 @@
 FUNCTION = ENV.fetch("FUNCTION") {ENV.fetch("K_SERVICE")}
-VERSION = ENV.fetch("VERSION") {ENV.fetch("K_SERVICE")}
+VERSION = ENV.fetch("VERSION") {ENV.fetch("K_REVISION").split(".").last}
 PRODUCTION = ENV.fetch("DEPLOY_ENV") == "production"
 
-IO = if defined?(logger) then logger else $stdout end
 SemanticLogger.default_level = :trace
 SemanticLogger.add_signal_handler
-SemanticLogger.add_appender(io: IO, formatter: :one_line)
-SemanticLogger.add_appender(io: logger, formatter: :one_line)
+SemanticLogger.add_appender(io: $stdout, formatter: :one_line) unless defined?(logger)
+SemanticLogger.add_appender(io: logger, formatter: :one_line) if defined?(logger)
 LOGGER = SemanticLogger[FUNCTION]
 LOGGER.info("Using #{if defined?(logger) then "logger" else "stdout" end}")
 
