db122a2a62d9c473c7367049691f7e9d69b4f1cc
Sarah Nyberg <srhbutts@gmail.com>
2019-05-28 00:25:33 +0000

    adding missing router keyword

--- mtgjson-import/function.rb
+++ mtgjson-import/function.rb
@@ -1,4 +1,4 @@
-def function(response:, database:)
+def function(response:, router:, database:)
   cards = database.relations.fetch(:cards)
   response.write(cards.count.to_s)
   response.status = :ok
