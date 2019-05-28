77634a8a0d619ce2b7466215af4c9cc0dd371758
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-05-28 01:15:01 -0700

    Don't need these

--- _base/configuration.rb
+++ _base/configuration.rb
@@ -5,9 +5,7 @@ PRODUCTION = ENV.fetch("DEPLOY_ENV") == "production"
 SemanticLogger.default_level = :trace
 SemanticLogger.add_signal_handler
 SemanticLogger.add_appender(io: $stdout, formatter: :one_line) unless defined?(logger)
-SemanticLogger.add_appender(io: logger, formatter: :one_line) if defined?(logger)
 LOGGER = SemanticLogger[FUNCTION]
-LOGGER.info("Using #{if defined?(logger) then "logger" else "stdout" end}")
 
 if PRODUCTION
   Google::Cloud::Debugger.configure do |config|
