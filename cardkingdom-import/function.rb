68665572612bddfc7664f451c0f820eadcfb0f0c
Sarah Nyberg <srhbutts@gmail.com>
2019-05-31 03:31:55 +0000

    cleaned up categories, started buylist parsing

--- cardkingdom-import/function.rb
+++ cardkingdom-import/function.rb
@@ -9,12 +9,34 @@ def update_retail
 end
 
 def categories
-    url = "https://cardkingdom.com/search/mtg"
+  url = "https://cardkingdom.com/search/mtg"
 
-    raw = Nokogiri::HTML(Net::HTTP.get(URI(url)))
-    sets = []
-    raw.css('div#editionContainer').css('option').each do |set|
-        sets.push set["value"].to_i, set.children.text
-    end
-    sets
+  raw = Nokogiri::HTML(Net::HTTP.get(URI(url)))
+  sets = []
+  discard = ["All Editions", "Standard", "Modern"]
+  raw.css('div#editionContainer').css('option').each do |set|
+    if !(discard.include?(set.children.text))
+      sets.push set["value"].to_i, set.children.text.strip
+  end
+  sets
+end
+
+def parse_buylist(url)
+  puts "parsing #{url}\n"
+
+  raw = Nokogiri::HTML(Net::HTTP.get(URI(url)))
+  raw.css('div.itemContentWrapper').each do |card|
+
+    cardname     = card.css('span.productDetailTitle').inner_text
+    raw_set      = card.css('div.productDetailSet').css('a').inner_text.strip
+    cash_dollars = card.css('.usdSellPrice').css('span.sellDollarAmount').inner_text
+    cash_cents   = card.css('.usdSellPrice').css('span.sellCentsAmount').inner_text
+    parsed_set   = raw_set.split(/\((C|U|R|M|P|S)\)/)
+    set          = parsed_set[0]
+    foil         = parsed_set[2]
+    foil         = (foil == nil) ? "0" : "1"
+    cash         = cash_dollars + "." + cash_cents
+
+    puts cardname, set, cash, "foil: " + foil, "\n"
+  end
 end
