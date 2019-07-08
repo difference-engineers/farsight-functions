ef3c0b70f775d0d194e4285699ab2e81106a5a68
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-07-07 20:05:14 -0700

    Adding sorbet and rubocop, but also centralizing gems

--- _base/boot.rb
+++ _base/boot.rb
@@ -5,4 +5,4 @@ require "bundler"
 $stdout.sync = true
 $stderr.sync = true
 
-Bundler.require(:default, ENV.fetch("DEPLOY_ENV", "development"))
+Bundler.require(:default, ENV.fetch("DEPLOY_ENV", "development"), ENV.fetch("SERVICE").to_sym)
