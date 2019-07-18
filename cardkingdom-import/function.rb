9570bb78913433f8d9f1993c9ccb369c84cdad8c
Sarah Nyberg <srhbutts@gmail.com>
2019-07-18 22:23:59 +0000

    rewrote categories function to avoid syntax errors

--- cardkingdom-import/function.rb
+++ cardkingdom-import/function.rb
@@ -20,50 +20,44 @@ def update_retail
   end
 end
 
-def categories
+def categories()
   url = "https://cardkingdom.com/search/mtg"
 
   raw = Nokogiri::HTML(Net::HTTP.get(URI(url)))
   sets = []
   discard = ["All Editions", "Standard", "Modern"]
   raw.css("div#editionContainer").css("option").each do |set|
-    unless discard.include?(set.children.text)
-      sets.push(Integer(set["value"], 10), set.children.text.strip)
+    sets.push(Integer(set["value"], 10), set.children.text.strip) unless discard.include?(set.children.text)
   end
-    sets
-  end
-  sets
 end
 
 def buylist_category_urls
   urls = []
 
-   base_url = "https://cardkingdom.com/purchasing/mtg_singles?filter%5Bipp%5D=100&filter%5Bsort%5D=name&filter%5Bsearch%5D=mtg_advanced\
+  base_url = "https://cardkingdom.com/purchasing/mtg_singles?filter%5Bipp%5D=100&filter%5Bsort%5D=name&filter%5Bsearch%5D=mtg_advanced\
                &filter%5Bname%5D=&filter%5Bcategory_id%5D=#{category_id}&filter%5Bfoil%5D=1&filter%5Bnonfoil%5D=1&filter%5Bprice_op%5D=&filter%5Bprice%5D="
 end
 
 def parse_buylist(url)
+  vendor = find(:vendors, :slug => "cardkingdom")
 
-  def parse_buylist(url)
-    vendor = find(:vendors, :slug => "cardkingdom")
+  raw = Nokogiri::HTML(Net::HTTP.get(URI(url)))
+  raw.css("div.itemContentWrapper").each do |card|
+    cardname = card.css("span.productDetailTitle").inner_text
+    raw_set      = card.css("div.productDetailSet").css("a").inner_text.strip
+    cash_dollars = card.css(".usdSellPrice").css("span.sellDollarAmount").inner_text
+    cash_cents   = card.css(".usdSellPrice").css("span.sellCentsAmount").inner_text
+    parsed_set   = raw_set.split(/\((C|U|R|M|P|S)\)/)
+    set          = parsed_set[0]
+    foil         = parsed_set[2]
+    foil.nil? ? foil = "0" : foil = "1"
+    cash         = cash_dollars + "." + cash_cents
+    sell         = monetize.parse(cash)
 
-    raw = Nokogiri::HTML(Net::HTTP.get(URI(url)))
-    raw.css("div.itemContentWrapper").each do |card|
-      cardname = card.css("span.productDetailTitle").inner_text
-      raw_set      = card.css("div.productDetailSet").css("a").inner_text.strip
-      cash_dollars = card.css(".usdSellPrice").css("span.sellDollarAmount").inner_text
-      cash_cents   = card.css(".usdSellPrice").css("span.sellCentsAmount").inner_text
-      parsed_set   = raw_set.split(/\((C|U|R|M|P|S)\)/)
-      set          = parsed_set[0]
-      foil         = parsed_set[2]
-      foil.nil? ? foil = "0" : foil = "1"
-      cash         = cash_dollars + "." + cash_cents
-      sell         = monetize.parse(cash)
+    card = find(table, {})
+    insert(:card_sell_prices, card, :sell_cents => sell.cents, :sell_currency => sell.currency, :reported_condition => reported_condition, :vendor => vendor)
+end
 
-      card = find(table, {})
-      insert(:card_sell_prices, card, :sell_cents => sell.cents, :sell_currency => sell.currency, :reported_condition => reported_condition, :vendor => vendor)
-    end
-  end
 
   def parse_retail(url)
 
