8d0db2ee596f9e3df2a701f2cfbeafdbe1f2ce72
Sarah Nyberg <srhbutts@gmail.com>
2019-07-18 22:02:47 +0000

    added basic test request with env-based auth token

--- tcgplayer-api-import/function.rb
+++ tcgplayer-api-import/function.rb
@@ -1,3 +1,15 @@
 def function(response:, router:, database:)
   response.status = :ok
 end
+
+require 'uri'
+require 'net/http'
+
+url = URI("http://api.tcgplayer.com/v1.27.0/app/authorize/" + ENV["TCGPLAYER_ACCESS_TOKEN"])
+
+http = Net::HTTP.new(url.host, url.port)
+
+request = Net::HTTP::Post.new(url)
+
+response = http.request(request)
+puts response.read_body
