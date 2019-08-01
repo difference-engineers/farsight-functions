0b06db3ce7957730f8d276fd847461edf486b106
Sarah Nyberg <srhbutts@gmail.com>
2019-08-01 22:10:36 +0000

    start of set_ids function

--- cardmarket-import/function.rb
+++ cardmarket-import/function.rb
@@ -1,3 +1,13 @@
 def function(response:, router:, database:)
   response.status = :ok
 end
+
+def set_ids
+  url = "https://www.cardmarket.com/en/Magic/Products/Singles"
+
+  raw = Nokogiri::HTML(Net::HTTP.get(URI(url)))
+  raw.css('select[name=idExpansion]').css('option').each do |this|
+
+  end
+
+end
