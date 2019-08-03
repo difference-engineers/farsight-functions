b62948645628c96a19923e2d9216301a5cbc4b2f
Sarah Nyberg <srhbutts@gmail.com>
2019-08-03 22:04:15 +0000

    fixed syntax error

--- tcgplayer-api-import/function.rb
+++ tcgplayer-api-import/function.rb
@@ -1,11 +1,13 @@
 def function(response:, router:, database:)
   response.status = :ok
+end
 
+def generate_token
   uri = URI.parse("https://api.tcgplayer.com/token")
   request = Net::HTTP::Post.new(uri)
   request.set_form_data(
-    "client_id" => $ENV["TCGPLAYER_CLIENT_ID"]
-    "client_secret" => $ENV["TCGPLAYER_CLIENT_SECRET"],
+    "client_id" => ENV["TCGPLAYER_CLIENT_ID"],
+    "client_secret" => ENV["TCGPLAYER_CLIENT_SECRET"],
     "grant_type" => "client_credentials",
   )
 
@@ -16,5 +18,5 @@ def function(response:, router:, database:)
   response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
     http.request(request)
   end
+  binding.pry
 end
-
