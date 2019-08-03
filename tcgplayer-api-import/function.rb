711095d24d68de4a4015e4a8f32cdea306eb75b9
Sarah Nyberg <srhbutts@gmail.com>
2019-08-03 21:22:00 +0000

    request auth token with env variables

--- tcgplayer-api-import/function.rb
+++ tcgplayer-api-import/function.rb
@@ -1,15 +1,20 @@
 def function(response:, router:, database:)
   response.status = :ok
+
+  uri = URI.parse("https://api.tcgplayer.com/token")
+  request = Net::HTTP::Post.new(uri)
+  request.set_form_data(
+    "client_id" => $ENV["TCGPLAYER_CLIENT_ID"]
+    "client_secret" => $ENV["TCGPLAYER_CLIENT_SECRET"],
+    "grant_type" => "client_credentials",
+  )
+
+  req_options = {
+    use_ssl: uri.scheme == "https",
+  }
+
+  response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+    http.request(request)
+  end
 end
 
-require 'uri'
-require 'net/http'
-
-url = URI("http://api.tcgplayer.com/v1.27.0/app/authorize/" + ENV["TCGPLAYER_ACCESS_TOKEN"])
-
-http = Net::HTTP.new(url.host, url.port)
-
-request = Net::HTTP::Post.new(url)
-
-response = http.request(request)
-puts response.read_body
