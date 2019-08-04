4301d7cdfdf633b2b97b0e911e3212ac9449b88a
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-08-03 19:59:34 -0700

    Adding logging

--- mtgjson-import-cards/function.rb
+++ mtgjson-import-cards/function.rb
@@ -1,13 +1,19 @@
 # typed: true
 def function(request:, response:, database:)
   uri = URI("https://mtgjson.com/json/AllCards.json")
+
+  LOGGER.info("Fetching cards")
   document = Net::HTTP.get(uri)
+
+  LOGGER.info("Parsing JSON")
   json = JSON.parse(document)
 
+  LOGGER.info("Getting table")
   mtgjson_imports = database.relations.fetch("mtgjson_imports")
   changeset = mtgjson_imports.changeset(:create, "type" => "AllCards", "imported_at" => Time.now, "raw" => json)
   changeset.commit
 
+  LOGGER.info("Finishing")
   response.write(mtgjson_imports.count.to_s)
   response.status = :ok
 end
