b7c91feec6bdb4077fc516997618d17d4c1aba07
Sarah Nyberg <srhbutts@gmail.com>
2019-08-04 21:55:17 +0000

    first sample request added-- fetches all categories

--- tcgplayer-api-import/function.rb
+++ tcgplayer-api-import/function.rb
@@ -1,6 +1,9 @@
 def function(response:, router:, database:)
   response.status = :ok
   token = generate_token()
+
+  response = sample_request("http://api.tcgplayer.com/v1.32.0/catalog/categories/1/groups", token)
+  JSON.parse(response.read_body)
 end
 
 def generate_token
@@ -20,4 +23,18 @@ def generate_token
     http.request(request)
   end
   JSON.parse(response.read_body)["access_token"]
+end
+
+def sample_request(request_url, bearer_token)
+  uri = URI.parse(request_url)
+  request = Net::HTTP::Get.new(uri)
+  request["Authorization"] = "Bearer " + bearer_token
+
+  req_options = {
+    use_ssl: uri.scheme == "https",
+  }
+
+  response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+    http.request(request)
+  end
 end
