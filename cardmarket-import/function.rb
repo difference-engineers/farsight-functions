774e05d4c6f6a35d1c26f60ef6040da96ca6feee
Sarah Nyberg <srhbutts@gmail.com>
2019-08-05 22:06:44 +0000

    changed to map

--- cardmarket-import/function.rb
+++ cardmarket-import/function.rb
@@ -7,8 +7,9 @@ def set_ids
 
   url = "https://www.cardmarket.com/en/Magic/Products/Singles"
 
+  discard = ["All"]
   raw = Nokogiri::HTML(Net::HTTP.get(URI(url)))
-  raw.css('select[name=idExpansion]').css('option').each do |this|
-    binding.pry
-  end
+  raw.css('select[name=idExpansion]').css('option').map do |set|
+    (Integer(set["value"], 10)) unless discard.include?(set.children.text)
+  end.compact
 end
