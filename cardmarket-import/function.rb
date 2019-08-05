4200f69cf896aebce6af71f4c29b857ebe7505f0
Sarah Nyberg <srhbutts@gmail.com>
2019-08-05 21:35:53 +0000

    added logger

--- cardmarket-import/function.rb
+++ cardmarket-import/function.rb
@@ -3,11 +3,12 @@ def function(response:, router:, database:)
 end
 
 def set_ids
+  LOGGER.info("Getting cardmarket's set ids")
+
   url = "https://www.cardmarket.com/en/Magic/Products/Singles"
 
   raw = Nokogiri::HTML(Net::HTTP.get(URI(url)))
   raw.css('select[name=idExpansion]').css('option').each do |this|
-
+    binding.pry
   end
-
 end
