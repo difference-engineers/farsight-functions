ba221ee2d47e23cbd04e8edf9f3ef30c2a6b0649
Sarah Nyberg <srhbutts@gmail.com>
2019-08-10 19:57:44 +0000

    added logger to detect invalid urls passed to every_page

--- cardmarket-import/function.rb
+++ cardmarket-import/function.rb
@@ -18,17 +18,15 @@ def set_slugs
 end
 
 def every_page(set_url)
+  LOGGER.error("Invalid set url passed, requires the base set url without query strings.")
   raw = Nokogiri::HTML(Net::HTTP.get(URI(set_url)))
   return_urls = []
 
-  #still need handling for pages with no pagination, and for urls without site=x
-  #doesn't work when we're on the same &page= as in the url_format line
-
-  url_format = "https://www.cardmarket.com"
-  url_format += raw.css("div#pagination").css("a.dropdown-item")[1].attributes["href"].text
-
+  url_format = set_url
+  url_format += "?perSite=20&site=#{raw.css("div#pagination").css("a.dropdown-item").count.to_s}"
+  binding.pry
   raw.css("div#pagination").css("a.dropdown-item").map do |page|
-    url_format.gsub(/site=[0-9]/, "site=#{page.children.text}")
+    url_format.gsub(/site=[0-9]+/, "site=#{page.children.text}")
   end
 end
 
@@ -38,7 +36,6 @@ def scrape_set(set_url)
     raw = Nokogiri::HTML(Net::HTTP.get(URI(page)))
     raw.css('div[id^="productRow"]').each do |card|
       card_data = Hash.new
-
       card_data["card_url"] = card.css("a")[1]["href"]
       card_data["card_name"] = card.css("div.col-10.col-md-8.px-2.flex-column.align-items-start.justify-content-center").text
       card_data["card_set_number"] = card.css("div.col-md-2.d-none.d-lg-flex.has-content-centered").text
@@ -54,10 +51,9 @@ end
 def scrape_card_page(url)
   LOGGER.info "Scraping card data from #{url}"
   raw = Nokogiri::HTML(Net::HTTP.get(URI(url)))
-  binding.pry
   card_data = Hash.new
   seller_inv_data = Hash.new
-
+  binding.pry
   # basic info
   card_data["available_inventory"] = raw.css("#tabContent-info > div > div.col-12.col-lg-6.mx-auto > div > div.info-list-container.col-12.col-md-8.col-lg-12.mx-auto.align-self-start > dl > dd:nth-child(8)").text
   card_data["lowest_eur"] = raw.css("#tabContent-info > div > div.col-12.col-lg-6.mx-auto > div > div.info-list-container.col-12.col-md-8.col-lg-12.mx-auto.align-self-start > dl > dd:nth-child(10)").text
@@ -67,15 +63,16 @@ def scrape_card_page(url)
   card_data["one_day_average_price"] = raw.css("#tabContent-info > div > div.col-12.col-lg-6.mx-auto > div > div.info-list-container.col-12.col-md-8.col-lg-12.mx-auto.align-self-start > dl > dd:nth-child(18) > span").text
 
   # foil info
-    # repeat card data for foils only, if available
+  # repeat card data (trending by day, etc) for foils only, if available?
+  raw.css('div[id^="articleRow"]').each do |row|
+    seller_inv_data["seller_name"] = row[1].css("span.mr-1 > a").text
+    seller_inv_data["seller_country"] = row[1].css("span.mr-1")[1]["title"]
+    #seller_inv_data["seller_condition"]
+    #seller_inv_data["card_language"]
+    seller_inv_data["eur_price"] = row[1].css("div.flex-column").first.text
+    #seller_inv_data["is_foil"]
+    seller_inv_data["qty_available"] = row.("span.mr-3").text
+    #seller_inv_data["seller_type"]
+  end
 
-  # seller inventories
-  seller_inv_data["seller_name"]
-  seller_inv_data["seller_country"]
-  seller_inv_data["seller_condition"]
-  seller_inv_data["card_language"]
-  seller_inv_data["eur_price"]
-  seller_inv_data["is_foil"]
-  seller_inv_data["qty_available"]
-  seller_inv_data["seller_type"] # powerseller, professional, private
 end
