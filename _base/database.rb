694b9bde062279162f16b49d9092cf521c88cba1
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-08-11 20:34:05 -0700

    Avoid using this constant and also lets not autorun migrations

--- _base/database.rb
+++ _base/database.rb
@@ -1,5 +1,5 @@
 # typed: ignore
-CONFIGURATION = {
+DATABASE = ROM.container(:sql, ENV.fetch("POSTGRES_URI"), {
   :encoding => "UTF8",
   :username => ENV.fetch("POSTGRES_USERNAME"),
   :password => ENV.fetch("POSTGRES_PASSWORD"),
@@ -18,18 +18,14 @@ CONFIGURATION = {
     :connection_expiration,
     :connection_validator,
   ],
-}.freeze
-DATABASE = ROM.container(:sql, ENV.fetch("POSTGRES_URI"), CONFIGURATION) do |let|
+}) do |let|
   let.gateways.fetch(:default).use_logger(LOGGER)
-  let.gateways.fetch(:default).run_migrations if let.gateways.fetch(:default).pending_migrations?
 
-  unless let.gateways.fetch(:default).pending_migrations?
-    let.relation(:cards) do
-      schema(:infer => true)
-    end
+  let.relation(:cards) do
+    schema(:infer => true)
+  end
 
-    let.relation(:mtgjson_imports) do
-      schema(:infer => true)
-    end
+  let.relation(:mtgjson_imports) do
+    schema(:infer => true)
   end
 end
