80985443352d20980b41f7e1a238940501adc761
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-08-11 20:34:21 -0700

    Allow for reloading

--- _base/configuration.rb
+++ _base/configuration.rb
@@ -2,11 +2,18 @@
 SERVICE = ENV.fetch("SERVICE") {ENV.fetch("K_SERVICE")}
 VERSION = ENV.fetch("VERSION") {ENV.fetch("K_REVISION").split(".").last}
 PRODUCTION = ENV.fetch("DEPLOY_ENV") == "production"
+CONSOLE = !!ENV.fetch("CONSOLE", false)
 
 SemanticLogger.default_level = :trace
 SemanticLogger.add_signal_handler
 SemanticLogger.add_appender(:io => $stdout, :formatter => :one_line) unless defined?(logger)
 LOGGER = SemanticLogger[SERVICE]
+
+if CONSOLE
+  def Pry.reload!
+    $LOADED_FEATURES.grep(/#{SERVICE}/).reject{|file| file.match?(/boot|config|application/)}.each(&method(:load))
+  end
+end
 
 if PRODUCTION
   Google::Cloud::Debugger.configure do |config|
